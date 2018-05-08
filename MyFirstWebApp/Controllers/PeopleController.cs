51476ca97e8cd59f69d7b18bbbe63f8a30b676ef
deltagare <deltagare@LEXJONKAF19>
2018-05-08 16:26:22 +0200

    init

--- MyFirstWebApp/Controllers/PeopleController.cs
+++ MyFirstWebApp/Controllers/PeopleController.cs
@@ -1,7 +1,8 @@
 ﻿using MyFirstWebApp.Models;
-using System;
+using System; 
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc; 
 
@@ -13,36 +14,48 @@ namespace MyFirstWebApp.Controllers
         //GET: People
         public ActionResult People()
         {
-
-            Person.DBPeople.Add(new Person { Name = "Fakhara", PhoneNO = "0721237605", City = "Växjö" });
-            Person.DBPeople.Add(new Person { Name = "Sadiqa", PhoneNO = "0721567625", City = "Malmo" });
-            Person.DBPeople.Add(new Person { Name = "Hina", PhoneNO = "072122775", City = "Älmult" });
-            Person.DBPeople.Add(new Person { Name = "Maryyram", PhoneNO = "0721278984", City = "Växjö" });
-            Person.DBPeople.Add(new Person { Name = "Erum", PhoneNO = "0721237605", City = "Vitlanda" });
-            Person.DBPeople.Add(new Person { Name = "Rafia", PhoneNO = "0721456789", City = "Växjö" });
-            Person.DBPeople.Add(new Person { Name = "Wajeeha", PhoneNO = "0729876542", City = "Lamhult" });
-            Person.DBPeople.Add(new Person { Name = "Huma", PhoneNO = "07232244567", City = "Vislanda" });
-            Person.DBPeople.Add(new Person { Name = "Nazish", PhoneNO = "0726643219", City = "Sandbro" });
-            Person.DBPeople.Add(new Person { Name = "Adeela", PhoneNO = "0721237605", City = "Växjö" });
-            Person.DBPeople.Add(new Person { Name = "Farzana", PhoneNO = "0721237605", City = "Växjö" });
-            Person.DBPeople.Add(new Person { Name = "Sadia", PhoneNO = "0721567625", City = "Malmo" });
-            Person.DBPeople.Add(new Person { Name = "Hina", PhoneNO = "072122775", City = "Älmult" });
-            Person.DBPeople.Add(new Person { Name = "Misbah", PhoneNO = "0721278984", City = "Växjö" });
-            Person.DBPeople.Add(new Person { Name = "Amara", PhoneNO = "0721237605", City = "Vitlanda" });
-            Person.DBPeople.Add(new Person { Name = "Rahat", PhoneNO = "0721456789", City = "Växjö" });
-            Person.DBPeople.Add(new Person { Name = "Nabeeha", PhoneNO = "0729876542", City = "Lamhult" });
-            Person.DBPeople.Add(new Person { Name = "Humna", PhoneNO = "07232244567", City = "Vislanda" });
-            Person.DBPeople.Add(new Person { Name = "Naila", PhoneNO = "0726643219", City = "Sandbro" });
-            Person.DBPeople.Add(new Person { Name = "Amna", PhoneNO = "0721237605", City = "Växjö" });
+            Person me = new Person();
+            me.personlist();
+     
             return View(Person.DBPeople);
         }
         
-         public ActionResult Search(string searching,string sorting)
+    
+         public ActionResult Search(string Name, string City)
          {
-            return View("People", Person.DBPeople.Where(m => m.Name.StartsWith(searching)|| m.City == searching).ToList());
-            
+            if(!string.IsNullOrEmpty(Name))
+            {
+                return View("People", Person.DBPeople.Where(m => m.Name.Contains(Name)));
+            }
+            if(!string.IsNullOrEmpty(City))
+            {
+                return View("People", Person.DBPeople.Where(m => m.City.Contains(City)));
+            }
+            return View();
          }
-
+        public ActionResult Sort(string sortOrder)
+        {
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "Name_desc" : "";
+            ViewBag.CitySortParm = sortOrder == "City" ? "City_desc" : "City";
+            var person = from m in Person.DBPeople select m;
+            switch(sortOrder)
+            {
+                case "Name_desc":
+                 person = person.OrderByDescending(m => m.Name);
+                    break;
+                case "City":
+                  person= person.OrderBy(m => m.City);
+                    break;
+                case "City_desc":
+                    person =person.OrderByDescending(m => m.City);
+                    break;
+                default:
+                   person = person.OrderBy(m => m.Name);
+                    break;
+            }
+            return View("People",person);
+        }
+        
         [HttpGet]
         public ActionResult Create()
         {
@@ -50,46 +63,24 @@ namespace MyFirstWebApp.Controllers
         }
         
         [HttpPost]
-         public ActionResult Create(string Name, string PhoneNO,string City )
+        [ValidateAntiForgeryToken]
+         public ActionResult Create([Bind(Include = "Id,Name, PhoneNO,City")]Person person )
          {
-            Person person = new Person();
-            person.Name = Name;
-            person.PhoneNO = PhoneNO;
-            person.City = City;
-            Person.DBPeople.Add(person);
-            return View("Create",Person.DBPeople);
-           // return RedirectToAction("People");
-            
-         }
-        /*[HttpPost]
-        public ActionResult Index(Person person)
-        {
-            if (ModelState.IsValid)
+            if(ModelState.IsValid)
             {
+
                 Person.DBPeople.Add(person);
                 return RedirectToAction("People");
             }
             return View(person);
-
-        }*/
-
-        [HttpGet]
-        public ActionResult Remove(String Name)
+           
+           
+         }
+     public ActionResult Delete(int id)
         {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult Remove(String Name, List<Person> DBPeople)
-        {
-           try
-            {
-                return RedirectToAction("People");
-            }
-            catch
-            {
-                return View("Remove");
-            }
-        }
-
+            Person person = Person.DBPeople.SingleOrDefault(m => m.Id == id);
+            Person.DBPeople.Remove(person);
+            return RedirectToAction("People");
+        }   
     }
 }
--- MyFirstWebApp/Models/Person.cs
+++ MyFirstWebApp/Models/Person.cs
@@ -9,8 +9,10 @@ namespace MyFirstWebApp.Models
     public class Person
     {
 
-        
+        static int idCount = 0;
         public static List<Person> DBPeople = new List<Person>();
+        [Key]
+        public int Id { get; set; }
         [Required(ErrorMessage ="Person Name is Mandatory")]
         [Display(Name =" Person Name")]
         public string Name { get; set; }
@@ -20,10 +22,26 @@ namespace MyFirstWebApp.Models
         [Required(ErrorMessage ="City Name is Mandatory")]
         [Display(Name ="City")]
         public string City { get; set; }
-        
+        public Person()
+        {
+            Id = idCount++;
+            
+        }
+        public void personlist()
+        {
+            if (Id == 0)
+            {
+                Person.DBPeople.Add(new Person { Name = "Fakhara", PhoneNO = "0721237605", City = "Växjö" });
+                Person.DBPeople.Add(new Person { Name = "Sadiqa", PhoneNO = "0721567625", City = "Malmo" });
+                Person.DBPeople.Add(new Person { Name = "Hina", PhoneNO = "072122775", City = "Älmult" });
+                Person.DBPeople.Add(new Person { Name = "Maryyram", PhoneNO = "0721278984", City = "Växjö" });
+                Person.DBPeople.Add(new Person { Name = "Erum", PhoneNO = "0721237605", City = "Vitlanda" });
+                Person.DBPeople.Add(new Person { Name = "Rafia", PhoneNO = "0721456789", City = "Växjö" });
+                Person.DBPeople.Add(new Person { Name = "Wajeeha", PhoneNO = "0729876542", City = "Lamhult" });
+                Person.DBPeople.Add(new Person { Name = "Huma", PhoneNO = "07232244567", City = "Vislanda" });
+                Person.DBPeople.Add(new Person { Name = "Nazish", PhoneNO = "0726643219", City = "Sandbro" });
+            }
+        }
     }
     
-     
-
-
 }
